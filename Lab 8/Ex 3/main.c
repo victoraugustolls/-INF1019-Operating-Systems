c9c69e9bb6b3643f0cf55e965e0e4e1b9ea50658
Victor Souza <victoraugustolls@gmail.com>
2016-11-09 21:25:38 -0200

    Added third part from lab 8

--- Lab 8/Ex 3/main.c
+++ Lab 8/Ex 3/main.c
@@ -2,41 +2,59 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include "Queue.h"
 
 #define NUMTHREADS 2
+#define LIMIT 64
 
-int global;
+Queue* q;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; 
 
-void *contCrescente(void *threadid) {
-	for (int i = 0; i < 20; i++)
+void *produtor(void *threadid) {
+	for (int i = 0; i < LIMIT;)
 	{
-		global++;
-		printf("T1 (crescente): %d / valor global: %d\n", i+1, global);
 		sleep(1);
+		if(queueNotFull(q)) {
+			pthread_mutex_lock(&mutex);
+			int r = rand()%40;
+			enqueue(q, r);
+			printf("Produtor adicionou: %d\n", r);
+			pthread_mutex_unlock(&mutex);
+			i++;
+		}
+		else {
+			printf("Fila cheia!\n");
+		}
 	}
 	pthread_exit(NULL);
 }
 
-void *contDecrescente(void *threadid) {
-	for (int i = 30; i > 0; i--)
+void *consumidor(void *threadid) {
+	for (int i = 0; i < LIMIT;)
 	{
-		global++;
-		printf("T2 (decrescente): %d / valor global: %d\n", i, global);
 		sleep(2);
+		if(queueNotEmpty(q)) {
+			pthread_mutex_lock(&mutex);
+			int r = dequeue(q);
+			printf("Consumidor consumiu: %d\n", r);
+			pthread_mutex_unlock(&mutex);
+			i++;
+		}
+		else {
+			printf("Fila vazia!\n");
+		}
 	}
 	pthread_exit(NULL);
 }
 
 int main(int argc, char const *argv[])
 {
-	global = 0;
+	q = newQueue();
 
 	pthread_t threads[NUMTHREADS];
 
-	printf("valor inicial global: %d\n", global);
-
-	pthread_create(&threads[0], NULL, contCrescente, (void *)0);
-	pthread_create(&threads[1], NULL, contDecrescente, (void *)1);
+	pthread_create(&threads[0], NULL, produtor, (void *)0);
+	pthread_create(&threads[1], NULL, consumidor, (void *)1);
 
 	for (int i = 0; i < 2; i++)
 	{
