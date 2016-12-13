15412a334fafad5cd2822ab8c73b411bfe169b84
Gabriel Coutinho de Paula <gabrielcoutinhopaula@gmail.com>
2016-12-13 19:59:11 -0200

    Fixed udpclient and udpserver

--- T2/sample/udpclient.c
+++ T2/sample/udpclient.c
@@ -16,21 +16,24 @@
 /* 
  * error - wrapper for perror
  */
-void error(char *msg) {
+void error(char *msg) 
+{
     perror(msg);
     exit(0);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
     int sockfd, portno, n;
-    int serverlen;
+    unsigned int serverlen;
     struct sockaddr_in serveraddr;
     struct hostent *server;
     char *hostname;
     char buf[BUFSIZE];
 
     /* check command line arguments */
-    if (argc != 3) {
+    if (argc != 3) 
+    {
        fprintf(stderr,"usage: %s <hostname> <port>\n", argv[0]);
        exit(0);
     }
@@ -44,7 +47,8 @@ int main(int argc, char **argv) {
 
     /* gethostbyname: get the server's DNS entry */
     server = gethostbyname(hostname);
-    if (server == NULL) {
+    if (server == NULL) 
+    {
         fprintf(stderr,"ERROR, no such host as %s\n", hostname);
         exit(0);
     }
@@ -52,25 +56,29 @@ int main(int argc, char **argv) {
     /* build the server's Internet address */
     bzero((char *) &serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-	  (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+    bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
     serveraddr.sin_port = htons(portno);
 
-    /* get a message from the user */
-    bzero(buf, BUFSIZE);
-    printf("Please enter msg: ");
-    fgets(buf, BUFSIZE, stdin);
+    while(1) 
+    {
+        /* get a message from the user */
+        bzero(buf, BUFSIZE);
+        printf("Please enter msg: ");
+        fgets(buf, BUFSIZE, stdin);
 
-    /* send the message to the server */
-    serverlen = sizeof(serveraddr);
-    n = sendto(sockfd, buf, strlen(buf), 0, &serveraddr, serverlen);
-    if (n < 0) 
-      error("ERROR in sendto");
-    
-    /* print the server's reply */
-    n = recvfrom(sockfd, buf, strlen(buf), 0, &serveraddr, &serverlen);
-    if (n < 0) 
-      error("ERROR in recvfrom");
-    printf("Echo from server: %s", buf);
+        /* send the message to the server */
+        serverlen = sizeof(serveraddr);
+        n = sendto(sockfd, buf, strlen(buf), 0, (struct sockaddr *) &serveraddr, serverlen);
+        if (n < 0) 
+            error("ERROR in sendto");
+        
+        /* print the server's reply */
+        n = recvfrom(sockfd, buf, strlen(buf), 0, (struct sockaddr *) &serveraddr, &serverlen);
+        if (n < 0) 
+            error("ERROR in recvfrom");
+
+        printf("Echo from server: %s", buf);
+    }
+
     return 0;
 }
